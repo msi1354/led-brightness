b485914f2f753f90937e25854618cfd22ef28c50
msi1354 <86691276+msi1354@users.noreply.github.com>
2021-07-01 12:32:23 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    let x = 0
-    led.plot(x, 1)
+    led.plot(index, 2)
 })
+let x = 0
+let index = 0
+index = x
 for (let x = 0; x <= 4; x++) {
     led.plot(0, 0)
     led.plot(1, 0)
